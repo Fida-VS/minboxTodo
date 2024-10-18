fef05f4d7f065b3d317f4d45294849702e41347b
Fida-VS <poletaeva69vera@mail.ru>
2024-10-18 16:33:13 +0300

    add plate

--- src/components/control-panel/control-panel.tsx
+++ src/components/control-panel/control-panel.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hook';
 import { changeFilter } from '../../store/todoSlice';
 import styles from './control-panel.module.css';
@@ -10,8 +11,28 @@ type Todo = {
 
 export const ControlPanel: React.FC = () => {
 
+    const [countString, setCountString] = useState('');
+    const [plate, setPlate] = useState(false);
+    const [miniPlate, setMiniPlate] = useState(false);
+
     const dispatch = useAppDispatch();
+
     const todos = useAppSelector(state => state.todos.todos);
+
+    const onMouseEnterHandler = () => {
+        setMiniPlate(true);
+        setCountString(' items left');
+    };
+
+    const onMouseLeaveHandler = () => {
+        if(plate)return;
+        setCountString('');
+        setMiniPlate(false);
+    };
+
+    const togglePlate = () => {
+        setPlate(!plate);
+    };
 
     const getActiveTodosCounter = (todos: Todo[]) => {
       return todos.filter(todo => todo.completed === false).length;
@@ -20,20 +41,21 @@ export const ControlPanel: React.FC = () => {
     let activeTodosCount = getActiveTodosCounter(todos);
 
     return (
-        <div className={styles.controlPanel}>
+        <div className={`controlPanel ${plate ? 'controlPanel-plate' : ''}`} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} >
 
-            {/* <div className={styles.controlPanel__todosCount}>
-                <div>{activeTodosCount} items left</div>
+            <div className={`wrapper ${miniPlate ? 'miniPlate' : ''} ${plate ? 'plate' : ''}`}>
+
+            {plate && <div className={styles.controlPanel__buttons}>
+            <div onClick={() => dispatch(changeFilter('all'))}>All</div>
+            <div onClick={() => dispatch(changeFilter('active'))}>Active</div>
+            <div onClick={() => dispatch(changeFilter('completed'))}>Completed</div>
+            </div>}    
+
+        <div className={styles.controlPanel__text} onClick={togglePlate}>
+            <div>{activeTodosCount + countString}</div>
+            {(countString === ' items left') && <div className={styles.arrow}></div>}
             </div>
-
-            <div className={styles.controlPanel__buttons}>
-            <button type='button' onClick={() => dispatch(changeFilter('all'))}>All</button>
-            <button type='button' onClick={() => dispatch(changeFilter('active'))}>Active</button>
-            <button type='button' onClick={() => dispatch(changeFilter('completed'))}>Completed</button>
-            </div> */}
-
-            <div>{activeTodosCount}</div>
-            
+            </div>
         </div>
     )
 }
